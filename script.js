d7b81c68bd522b657f662602071a851c49917ea9
Ryan Acham <ryanacham@hotmail.com>
2022-05-27 10:03:46 -0400

    Inital Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// first, getting the current date
+var today = moment().format('LLLL'); 
+document.getElementById("date").innerHTML = today;
+console.log(today)
+
+function slotCol(){
+    if(moment().format('LT').substring(0,2)){
+
+    }
+}
+
