b5b73f1490f3a4bacab734ca21ac235959861c1c
Ryan Acham <ryanacham@hotmail.com>
2022-05-27 13:59:45 -0400

    adding timestamps and text input

--- script.js
+++ script.js
@@ -27,13 +27,33 @@ function checkHour() {
         var blockH = parseInt($(this).attr("id").split("hour")[1]);
         //check whether it is past present or future
         if (blockH < curHour) {
-
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
         }
         else if (blockH === curHour){
-
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
         }
         else{
-            
+            $(this).removeClass("past");
+            $(this).addClass("future");
+            $(this).removeClass("present");
         }
-})
+    })
+}
+
+checkHour();
+
+//check and load any local storage stuff
+$("#hour9 .event").val(localStorage.getItem("hour9"));
+$("#hour10 .event").val(localStorage.getItem("hour10"));
+$("#hour11 .event").val(localStorage.getItem("hour11"));
+$("#hour12 .event").val(localStorage.getItem("hour12"));
+$("#hour13 .event").val(localStorage.getItem("hour13"));
+$("#hour14 .event").val(localStorage.getItem("hour14"));
+$("#hour15 .event").val(localStorage.getItem("hour15"));
+$("#hour16 .event").val(localStorage.getItem("hour16"));
+$("#hour17 .event").val(localStorage.getItem("hour17"));
 
