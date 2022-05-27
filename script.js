c2e1a65763fae15ca93342fb56c973918223cdc0
Ryan Acham <ryanacham@hotmail.com>
2022-05-27 10:39:27 -0400

    Filling in and creating functions

--- script.js
+++ script.js
@@ -9,3 +9,31 @@ function slotCol(){
     }
 }
 
+//save button
+$(".save").on("click", function() {
+    //store user input
+    var userIn = $(this).siblings(".event").val();
+    //store time
+    var userTime = $(this).parent().attr("id");
+    //store to local storage
+    localStorage.setItem(userIn, userTime);
+})
+
+//check hour 
+function checkHour() {
+    var curHour = moment().hour();
+    //for each block
+    $(".block").each(function (){
+        var blockH = parseInt($(this).attr("id").split("hour")[1]);
+        //check whether it is past present or future
+        if (blockH < curHour) {
+
+        }
+        else if (blockH === curHour){
+
+        }
+        else{
+            
+        }
+})
+
